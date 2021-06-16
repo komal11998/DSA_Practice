f4a800d9df26ecd26854089e2213750a2d4ac73d
komal <chahliakomal777@gmail.com>
2021-06-16 17:51:10 +0530

    Print all the duplicates in the input string

---
+++ PrintDuplicates.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define NO_OF_CHARS 256
+
+using namespace std;
+
+void printDuplicate(string s)
+{
+  int count[NO_OF_CHARS] = {};
+  for (int i = 0; i < s.size(); i++)
+    count[s[i]]++;
+
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (count[s[i]] > 1)
+    {
+      cout << s[i] << "count = " << count[s[i]] << endl;
+      count[s[i]] = 0;
+    }
+  }
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  cout << "String is: " << s << endl;
+  printDuplicate(s);
+  return 0;
+}
